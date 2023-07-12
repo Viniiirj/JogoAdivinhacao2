035d15883e78ebdabe89b71881ba7584441f7ed5
Vinicius Soares Ramos <111201483+Viniiirj@users.noreply.github.com>
2023-07-11 21:11:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
-
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
@@ -16,15 +15,19 @@ function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
-  if (Number(inputNumber.value) == randomNumber) {
-    toggleScreen()
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `Acertou em ${xAttempts} tentativas`
+  if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 9) {
+    alert("Digite um numero entre 0 e 10")
   }
-  if(inputNumber.value !='') {
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen()
+      document.querySelector(
+        ".screen2 h2"
+      ).innerText = `Acertou em ${xAttempts} tentativas`
+    }
+  if (inputNumber.value != "") {
     xAttempts++
   }
+
   inputNumber.value = ""
 }
 function handleResetClick() {
@@ -36,6 +39,12 @@ function handleResetClick() {
 function toggleScreen() {
   screen1.classList.toggle("hide")
   screen2.classList.toggle("hide")
+}
+
+function enterScreen(e) {
+if (e.key == "Enter" && screen1.classList.contains("hide")) {
+  handleResetClick()
+}
 }
 
 function enterScreen(e) {
